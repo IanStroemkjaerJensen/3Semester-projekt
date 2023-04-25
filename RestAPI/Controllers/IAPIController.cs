ffbcc7ae897d674232f2c72123f27048d27bc8e7
IanStroemkjaerJensen <82367076+IanStroemkjaerJensen@users.noreply.github.com>
2023-04-25 13:40:03 +0200

    Add files via upload

---
+++ RestAPI/Controllers/IAPIController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    public interface IAPIController
+    {
+
+        ActionResult AddTopic(Topic topic);
+        ActionResult<IEnumerable<Topic>> GetTopic();
+        public ActionResult UpdateTopic(Topic topic);
+        public ActionResult DeleteTopic(Topic topic);
+
+    }
+}
